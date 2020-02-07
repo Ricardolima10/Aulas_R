9740d4153578c35d79d30e65378922d07e36de63
Ricardo Lima <ricardosem@gmail.com>
2020-02-07 16:06:34 -0300

    teste_git

---
+++ teste_banco_iris.R
@@ -0,0 +1,17 @@
+dados <- iris
+library(ggplot2)
+library(esquisse)
+
+library(dplyr)
+library(ggplot2)
+
+grafico <- dados %>%
+ filter(!(Species %in% "virginica")) %>%
+ ggplot() +
+ aes(x = Sepal.Length, y = Sepal.Width, colour = Species, size = Petal.Length) +
+ geom_point() +
+ geom_smooth(span = 0.44) +
+ scale_color_hue() +
+ theme_minimal() +
+ theme(legend.position = "bottom")
+grafico
